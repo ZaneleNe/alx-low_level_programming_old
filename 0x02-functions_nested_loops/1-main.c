db939feca93f2190b792d5395567976e86bcff90
ZaneleNe <zanelenema97@gmail.com>
2023-07-18 07:23:30 -0700

    added

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: check the code for ALX School students
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
