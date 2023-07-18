8f5a91c8184e3bc27559b440bb3a27841bafde9d
ZaneleNe <zanelenema97@gmail.com>
2023-07-18 09:02:54 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * printFib - prints the total numbers in fibonachi sequence
+ *
+ * @range: fibonachi length
+ * Return: nothing
+ */
+void printFib(unsigned long int range)
+{
+	static unsigned long int first = 0, second = 1, sum, i;
+
+	for (i = 1; i <= range; i++)
+	{
+		sum = first + second;
+		first = second;
+		second = sum;
+
+		printf("%lu", sum);
+
+		if (i < range)
+			printf(" ,");
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints 98 fibonachi values
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printFib(98);
+
+	return (0);
+}
