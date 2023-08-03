7bf7a2720414debebb50a9c7cf7a64f67adc2487
ZaneleNe <zanelenema97@gmail.com>
2023-08-03 15:17:07 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings can be
+ * considered identical, otherwise returns 0
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns either 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
+
