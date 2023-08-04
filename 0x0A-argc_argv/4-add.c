2319334ee81798e40c462c6a66e22945fe34bbb7
ZaneleNe <zanelenema97@gmail.com>
2023-08-04 13:45:20 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,60 +1,37 @@
+#include "main.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include "holberton.h"
 
 /**
- * main - programs main function
- * @argc: number of things typed in command line
- * @argv: array of string values for typed things
- * Return: returns the name of the program followed by a new line.
- * If you rename the program, it will print the new name,
- * without having to compile it again
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ * Return: 1 if error, 0 if function runs properly.
  */
+
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+	int total, i;
+	char *p;
+	int num;
 
+	total = 0;
 	if (argc > 1)
 	{
-		sum = contains_digits(argv, argc);
-		if (sum >= 0)
-			printf("%d\n", sum);
-		else
+		for (i = 1; argv[i]; i++)
 		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-	else
-	{
-		printf("0\n");
-	}
-	return (0);
-}
-
-/**
- * contains_digits - checks if an array only has digits
- * @argv: array to be checked
- * @argc: array size
- * Return: returns 1 if non digit found. Otherwise returns sum of numbers.
- */
-int contains_digits(char *argv[], int argc)
-{
-	int i, j, sum = 0, length;
-
-	for (i = 1; i < argc; i++)
-	{
-		length = strlen(argv[i]);
-		for (j = 0; j < length; j++)
-		{
-			if (isdigit(argv[i][j]))
-				continue;
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
 			else
-				return (-1);
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
 	}
-	return (sum);
+	printf("%d\n", total);
+	return (0);
 }
