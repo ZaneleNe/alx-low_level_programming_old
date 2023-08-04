28ec96bf40ad3749d827ede5bb7248b708dc27b4
ZaneleNe <zanelenema97@gmail.com>
2023-08-04 07:34:34 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - programs main function
+ * @argc: number of things typed in command line
+ * @argv: array of string values for typed things
+ * Return: returns the name of the program followed by a new line.
+ * If you rename the program, it will print the new name,
+ * without having to compile it again
+ */
+int main(int argc, char *argv[])
+{
+	/*To avoid unused parameter error cast argv to void*/
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
