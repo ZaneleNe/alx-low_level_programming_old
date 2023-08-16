fa4713051f5a8edf7c7acdbea45d30f971a6edfb
ZaneleNe <zanelenema97@gmail.com>
2023-08-16 14:14:45 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer that will be used as call back
+ *
+ * Return: NULL
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		(*f)(name);
+}
